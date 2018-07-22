7dd9d549b5998b17be54813bfd6daf78f6a3aa2a
dhuynh <dhuynh@dhgital.com>
2018-07-21 21:40:02 -0400

    define dir

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,29 +1,9 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
-  </head>
+<?php $page_title = 'Staff Menu'; ?>
+<?php include('../../private/shared/staff_header.php'); ?>
 
-  <body>
-    <header>
-      <h1>GBI Staff Area</h1>
-    </header>
+<div id="content">
+</div>
 
-    <navigation>
-      <ul>
-        <li><a href="index.php">Menu</a></li>
-      </ul>
-    </navigation>
-
-    <div id="content">
-    </div>
-
-    <footer>
-      &copy; <?php echo date('Y'); ?> Globe Bank
-    </footer>
-
-  </body>
-</html>
+<?php include('../../private/shared/staff_footer.php'); ?>
